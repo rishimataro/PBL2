f1e2e74f90075ae5b489e9ad608c7e483eaa7c8d
Nguyen Thai Ngoc Thao <ngocthaodana@gmail.com>
2024-11-06 10:51:06 +0700

    Comment code

--- src/Template/LinkedList.cpp
+++ src/Template/LinkedList.cpp
@@ -1,5 +1,8 @@
 #include <Template/LinkedList.hpp>
 
+/* template<class T>
+Node<T>::Node(T val) : data(val), next(NULL), prev(NULL) { }
+
 template<class T>
 LinkedList<T>::LinkedList() : head(NULL), count(0) { }
 
@@ -169,7 +172,7 @@ int LinkedList<T>::find(T value) {
 
 // Lấy Node: lấy giá trị tại vị trí index
 template<class T>
-T LinkedList<T>::get(int index) { 
+T LinkedList<T>::get(int index) const { 
     if (isEmpty() || index < 0 || index >= count) {
             throw out_of_range("Index out of range");
     }
@@ -205,7 +208,7 @@ void LinkedList<T>::set(int index, T value) {
 
 // Lấy kích thước
 template<class T>
-int LinkedList<T>::size() { 
+int LinkedList<T>::size() const { 
     return count;
 }
 
@@ -224,82 +227,4 @@ void LinkedList<T>::display() {
         current = current->next;
     } while (current != head);
     cout << endl;
-}
-
-
-/* int main() {
-    LinkedList<string> list;
-
-    // Thêm các chuỗi vào danh sách
-    list.append("apple");
-    list.display();  // Output: apple
-
-    list.prepend("orange");
-    list.display();  // Output: orange apple
-
-    list.append("banana");
-    list.display();  // Output: orange apple banana
-
-    // Thêm "grape" vào vị trí thứ 2 (giữa "orange" và "apple")
-    list.insertAt("grape", 2);
-    list.display();  // Output: orange apple grape banana
-
-    // Xóa phần tử ở vị trí thứ 2 (giá trị "grape")
-    list.remove(2);
-    list.display();  // Output: orange apple banana
-
-    // Tìm vị trí của "apple"
-    int pos = list.find("apple");
-    cout << "Vi tri cua 'apple': " << pos << endl;  // Output: 1
-
-    // Lấy giá trị tại vị trí thứ 0 (giá trị "orange")
-    cout << "Gia tri tai vi tri 0: " << list.get(0) << endl;  // Output: orange
-
-    string str;
-    cout << "Nhap chuoi: "; getline(cin, str);
-    list.append(str);
-    list.display();
-
-    LinkedList<int> list;
-
-    list.append(10); 
-    list.display();
-
-    list.prepend(5); 
-    list.display();
-
-    list.append(15); 
-    list.display();
-
-    list.insertAt(12, 1); 
-    list.display();
-
-    int n;
-    cout << "Nhap n: "; cin >> n;
-    if(list.find(n) == -1) cout << "Khong co!" << endl;
-    else cout << list.find(n) << endl; 
-
-    list.prepend(74); 
-    list.prepend(-8); 
-    list.prepend(0); 
-    list.prepend(54); 
-    list.prepend(17); 
-    
-    list.display();
-    list.removeFront();
-    list.display();
-
-    list.removeBack();
-    list.display();
-
-    cout << list.get(1) << ", " << list.get(2) << endl;
-    list.remove(1); 
-    list.display();
-
-    cout << list.get(1) << ", " << list.get(2) << endl;
-
-    list.clear(); 
-    list.display();
-
-    return 0;
 } */
