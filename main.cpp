3f30e993f27250dc05d4d0e03df5a8b90cd2c147
Nguyen Thai Ngoc Thao <ngocthaodana@gmail.com>
2024-11-14 15:05:48 +0700

    Delete Test

--- main.cpp
+++ main.cpp
@@ -22,46 +22,6 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     
-    listAccount listAcc;
-    listPatient listPat;
-    int check1 = listAcc.readListAccountFromFile();
-    
-
-    if(check1) cout << "Đọc file ACC thành công" << endl;
-    else cout << "Đọc file ACC thất bại" << endl;
-
-    // if(check2) cout << "Ghi file ACC thành công" << endl;
-    // else cout << "Ghi file ACC thất bại" << endl;
-
-    int check3 = listPat.readListPatientFromFile();
-    // int check4 = listPat.writeListPatientToFile(true);
-
-    if(check3) cout << "Đọc file PAT thành công" << endl;
-    else cout << "Đọc file PAT thất bại" << endl;
-
-    // if(check4) cout << "Ghi file PAT thành công" << endl;
-    // else cout << "Ghi file PAT thất bại" << endl;
-
-    // cout << "-------------------" << endl;
-    // vector<Account> acc = listAcc.setAllAccount();
-    // for(int i = 0; i < acc.size(); i++) {
-    //     cout << acc[i].getID() << " " << acc[i].getUserName() << " " << acc[i].getPassword() << " " << acc[i].getRole() << " " << acc[i].getID_patient() << endl;
-    // }    
-
-    // cout << "-------------------" << endl;
-    // vector<Account> acc1 = listAcc.setAccountByRole(1);
-    // for(int i = 0; i < acc1.size(); i++) {
-    //     cout << acc1[i].getID() << " " << acc1[i].getUserName() << " " << acc1[i].getPassword() << " " << acc1[i].getRole() << endl;
-    // }
-
-    // for(int i = 0; i < listAcc.size(); i++) {
-    //     cout << "ID_Patient: " << listAcc.get(i).getID_patient() << endl;
-    // }
-    int check2 = listAcc.writeListAccountToFile(true);
-    cout << "-------------------" << endl;
-    if(check2) cout << "Ghi file ACC thành công" << endl;
-    else cout << "Ghi file ACC thất bại" << endl;
-
 
     system("pause");
     return 0;
