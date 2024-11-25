90dd050c67daa7f894b41ae69cf8d0c909fa24c9
Nguyen Thai Ngoc Thao <ngocthaodana@gmail.com>
2024-11-25 14:26:04 +0700

    Fix: hàm write và update

--- include/Management/listMedicalRecord.hpp
+++ include/Management/listMedicalRecord.hpp
@@ -8,37 +8,37 @@ enum class SearchField_MR { RecordID, PatientID, Diagnosis};
 
 class listMedicalRecord : public LinkedList<MedicalRecord>
 {
-public:
-    listMedicalRecord();
-    ~listMedicalRecord();
+    public:
+        listMedicalRecord();
+        ~listMedicalRecord();
 
-    bool readListMedicalRecordFromFile();
-    bool writeListMedicalRecordToFile(bool check);
+        bool readListMedicalRecordFromFile();
+        bool writeListMedicalRecordToFile(bool check);
 
-    vector<MedicalRecord> setAllMedicalRecords();
-    vector<MedicalRecord> setMedicalRecordsByPatientID(const string& patientID);
-    vector<MedicalRecord> setMedicalRecordsByDiagnosis(const string& diagnosis);
-    vector<MedicalRecord> setMedicalRecordsByRecordRange(const string &startDate, const string &endDate);
-    vector<pair<string, int>> setDiagnosisCount(); // Thống kê số lượng chẩn đoán theo bệnh phổ biến
+        vector<MedicalRecord*> setAllMedicalRecords();
+        vector<MedicalRecord*> setMedicalRecordsByPatientID(const string& patientID);
+        vector<MedicalRecord*> setMedicalRecordsByDiagnosis(const string& diagnosis);
+        vector<MedicalRecord*> setMedicalRecordsByRecordRange(const string &startDate, const string &endDate);
+        vector<pair<string, int>> setDiagnosisCount(); // Thống kê số lượng chẩn đoán theo bệnh phổ biến
 
-    void addMedicalRecord(const string& patientID, const string& symptoms, const string& diagnosis, const string& dateOfRecord);
+        void addMedicalRecord(const string& patientID, const string& symptoms, const string& diagnosis, const string& dateOfRecord);
 
-    int checkRecordID(const string& recordID);
+        int checkRecordID(const string& recordID);
 
-    bool removeMedicalRecordByID(const string& recordID);
+        bool removeMedicalRecordByID(const string& recordID);
 
-    bool updateMedicalRecordByID(const string& recordID, const string& newDiagnosis, const string& newSymptoms);
+        bool updateMedicalRecordByID(const string& recordID, const string& newDiagnosis, const string& newSymptoms);
 
-    vector<MedicalRecord> searchMedicalRecord(SearchField_MR field, const string& value);
+        vector<MedicalRecord*> searchMedicalRecord(SearchField_MR field, const string& value);
 
-    // Phân tích các bệnh phổ biến và đưa ra giải pháp
-    string analyzeDiagnosisAndProvideSolutions(const string& diagnosis);
-    bool appendDiagnosisSolutionToFile(const string& diagnosis, const string& solution);
-    void loadSymptomSolutionsFromFile();
-    map<string, string> getSymptomSolutions();
+        // Phân tích các bệnh phổ biến và đưa ra giải pháp
+        string analyzeDiagnosisAndProvideSolutions(const string& diagnosis);
+        bool appendDiagnosisSolutionToFile(const string& diagnosis, const string& solution);
+        void loadSymptomSolutionsFromFile();
+        map<string, string> getSymptomSolutions();
 
-private:
-    map<string, string> symptomSolutions;
+    private:
+        map<string, string> symptomSolutions;
 };
 
 #endif
--- include/Management/listPatient.hpp
+++ include/Management/listPatient.hpp
@@ -20,9 +20,9 @@ class listPatient : public LinkedList<Patient>
         bool writeListPatientToFile(bool check); // lưu file tổng, check = true: lưu file tổng, check = false: lưu file lẻ
 
         //* Setter & Getter
-        vector<Patient> setPatientByGender(bool gender);
-        vector<Patient> setPatientByBirthRange(const string &startDate, const string &endDate);
-        vector<Patient> setAllPatient();
+        vector<Patient*> setPatientByGender(bool gender);
+        vector<Patient*> setPatientByBirthRange(const string &startDate, const string &endDate);
+        vector<Patient*> setAllPatient();
 
         //* Add
         void addPatient(const string& newFullName, const string& newPhone, const string& newDayOfBirth, const string& newCCCD, const string& newGender, const string& newAddress);
@@ -37,7 +37,7 @@ class listPatient : public LinkedList<Patient>
         bool updatePatientByID(const string& ID, const string& newFullName, const string& newPhone, const string& newDayOfBirth, const string& newCCCD, const string& newGender, const string& newAddress);
 
         //* Search
-        vector<Patient> searchPatient(SearchField field, const string& value);
+        vector<Patient*> searchPatient(SearchField field, const string& value);
 };
 
 #endif
