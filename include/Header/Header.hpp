824a4dbfc3b1f43ad0ace60134f182182a9503b6
Nguyen Thai Ngoc Thao <ngocthaodana@gmail.com>
2024-11-25 14:20:35 +0700

    Add: thư viện memory, functional, map, filesysteam; hàm safe_stoi

--- include/Header/Header.hpp
+++ include/Header/Header.hpp
@@ -8,15 +8,25 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-
-// #include <Library/Graphics.hpp>
-// #include <Library/Mouse.hpp>
+#include <memory>
+#include <functional>
+#include <filesystem>
 #define BASE_YEAR 1900
 using namespace std;
+namespace fs = std::filesystem;
 
 inline string toLowerCase(const string &str)
 {
     string result = str;
     transform(result.begin(), result.end(), result.begin(), ::tolower);
     return result;
+}
+
+inline int safe_stoi(const string &str) {
+    try {
+        return stoi(str);
+    } catch (const invalid_argument& e) {
+        cerr << "Lỗi: Chuỗi không hợp lệ để chuyển đổi thành số nguyên: " << e.what() << endl;
+        return 0;
+    }
 }
